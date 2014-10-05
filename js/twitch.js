4bd66c0fa22133812ce80f11b4172a345b479dcb
nityav <v.nitya789@gmail.com>
2014-10-05 00:33:08 -0500

    optimize code

--- js/twitch.js
+++ js/twitch.js
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>TWITCH </title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-    <link href="css/Twitch.css" rel="stylesheet" />
-    <!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-   <!-- <script type="text/javascript">
+
         function getJSONP(url, success) {
 
             var ud = '_' + +new Date,
@@ -119,49 +105,3 @@
         }
 
 
-    </script>-->
-</head>
-<body>
-    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <div class="container">
-            <div class="navbar-header heading">
-                <h1>
-                    TWITCH</h1>
-            </div>
-        </div>
-        <!-- /.container  -->
-    </div>
-    <!-- /.navbar -->
-    <div class="container">
-        <div class="row row-offcanvas row-offcanvas-right">
-            <div class="col-xs-12 col-sm-12 col-lg-12 jumbotron">
-                <div class="form-group col-lg-11 col-sm-6 col-xs-8">
-                    <input type="text" class="form-control" id="query" style="color: #888;" value="Search Query"
-                        onfocus="inputFocus(this)" onblur="inputBlur(this)" />
-                </div>
-                <input class="btn btn-primary col-lg-1" type="button" value="Search" onclick="search()" />
-                <div class="row col-xs-12 col-sm-12 col-lg-12">
-                    <div id="Summary" style="display: none">
-                        <div class="pull-left">
-                            Total results :<span id="total"></span></div>
-                        <div id="pagination" class="pull-right">
-                            <img src="images/arrow-left.png" id="arrow-left" onclick="prev()" />
-                            <span id="pageNumber"></span>
-                            <img src="images/arrow-right.png" id="arrow-right" onclick="Next()" />
-                        </div>
-                    </div>
-                </div>
-                <!--/row-->
-                <div id="error" class="row col-xs-12 col-sm-12 col-lg-12 text-warning">
-                </div>
-                <div id="results" class="row col-xs-12 col-sm-12 col-lg-12">
-                </div>
-            </div>
-            <!--/span-->
-        </div>
-        <!--/row-->
-        <hr />
-    </div>
-</body>
- <script type="text/javascript" src="js/twitch.js"></script>
-</html>
